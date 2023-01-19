9678bb065055350352b6c382c6037823fd50867d
viradeth xay-ananh <xayananh4@gmail.com>
2023-01-19 14:31:47 -0800

    pig latin takes sentence and converts each word into pig latin
    Co-authored-by: Charles <CharlesCookman@gmail.com>
    Co-authored-by: Mike Donovan <mike@mikedonovan.dev>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,50 @@
-//business logic
-function handleSubmission(userInput) {
 
+//business logic
+function convertWord(userInput) {
+  let newstring = "";
+  let firstLetter = userInput.charAt(0);
+  let secondLetter = userInput.charAt(1);
+
+  if (isVowel(firstLetter)) {
+    newstring = userInput.slice(1, userInput.length) + userInput.charAt(0) + "ay";
+    console.log(newstring);
+  } else if(userInput.slice(0, 2).toLowerCase() === "qu"){
+    newstring = userInput.slice(2, userInput.length) + userInput.slice(0, 2) + "ay";
+    console.log(newstring); 
+  } else if (!isVowel(firstLetter)) {
+    if (!isVowel(secondLetter)) {
+      newstring = userInput.slice(2, userInput.length) + userInput.slice(0, 2) + "ay";
+    console.log(newstring);
+    } else {
+      newstring = userInput.slice(1, userInput.length) + userInput.slice(0, 1) + "ay";
+    console.log(newstring);
+    }
+  }
+  return newstring;
 }
 
 //UI logic
 window.addEventListener("load", function(){
+  let userInput = document.getElementById("userInput");
+  let para = document.getElementById("result");
 
-let userInput = document.getElementById("userInput");
-let para = document.getElementById("result");
-
-document.querySelector("form#formWord").addEventListener("submit", function(event){
-
+  document.querySelector("form#formWord").addEventListener("submit", function(event){
+    event.preventDefault();
+    let convertedSentence = convertSentence(userInput.value);
+    para.innerText = convertedSentence;
+  });
 });
-});
+
+// Business logic
+
+function isVowel(letter) {
+  return 'aeiou'.includes(letter);
+}
+
+function convertSentence(sentence) {
+  let sentenceArray = sentence.split(" ");
+  let convertedArray = sentenceArray.map(function(word){
+    return convertWord(word);
+  })
+  return convertedArray.join(" ");
+}
---
+++ readme.md
@@ -0,0 +1,33 @@
+Describe pigLatin()
+
+TEST1: collect word from input 
+CODE: "test"
+EXPECTED RESULT: "test" 
+
+TEST2: Check if a word start with a
+CODE: "apple"
+EXPECTED RESULT: true
+
+TEST3: Check if a word starts with any vowel not y
+CODE: "umbrella"
+EXPECTED RESULT: "umbrellaay"
+
+TEST4: remove vowel off front and add to end
+CODE: "Open"
+EXPECTED RESULT: "penOay"
+
+TEST5: Check if words start with consonant
+CODE: "starts"
+EXPECTED RESULT: true
+
+TEST6: Remove up to two first consonants and put at end with ay
+CODE: "starts"
+EXPECTED RESULT: "artssay"
+
+TEST7: Check for qu
+CODE: "quit"
+EXPECTED RESULT: true
+
+TEST8: take qu off front and add quay to end
+CODE: "quit"
+EXPECTED RESULT: "itquay"
